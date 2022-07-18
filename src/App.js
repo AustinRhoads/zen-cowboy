3a5eec57d502e470ad5221eb79b08f13032ceacc
Austin Rhoads <austinrrhoads@ymail.com>
2022-07-18 15:38:06 -0500

    back again

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import {Routes, Route} from 'react-router-dom'
+
+import HackTools from './components/HackTools';
+import Home from './components/Home'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+      <h1>Zen Cowboy</h1>
+  
+      <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/hack-tools" element={<HackTools />}/>
+      </Routes>
+
+   
     </div>
   );
 }
---
+++ src/components/HackTools.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HackTools() {
+  return (
+    <div>HackTools</div>
+  )
+}
+
+export default HackTools
