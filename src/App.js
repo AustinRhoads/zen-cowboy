9c85bd3401195ee45df26621a4a2379b359850cb
Austin Rhoads <austinrrhoads@ymail.com>
2022-07-24 01:30:29 -0500

    navbar fonts

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import {Routes, Route} from 'react-router-dom'
 import './App.css';
 
 import Navbar from './components/Navbar';
+
 import Art from './components/pages/Art';
 import HackTools from './components/pages/HackTools';
 import Home from './components/pages/Home'
+import TechBuilds from './components/pages/TechBuilds';
+import WebDev from './components/pages/WebDev';
 
 
 
@@ -19,6 +22,8 @@ function App() {
           <Route path="/" element={<Home />}/>
           <Route path="/art" element={ <Art/> } />
           <Route path="/hacking-tools" element={<HackTools />}/>
+          <Route path="/webdev" element={<WebDev /> } />
+          <Route path="/tech-builds" element={ <TechBuilds /> } />
       </Routes>
 
    
